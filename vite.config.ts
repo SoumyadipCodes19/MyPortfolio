b8f0864dfbdd3768fdded2c87ceca5fa176a1447
SoumyadipCodes19 <soumyadip.ghosh19082004@gmail.com>
2025-06-09 19:12:25 +0530

    fix: simplify build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,41 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
   build: {
     outDir: 'dist',
-    sourcemap: false,
-    minify: 'terser',
-    cssMinify: true,
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true
-      }
-    },
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'framer-motion'],
-          icons: ['@heroicons/react', 'react-icons']
-        },
-        assetFileNames: (assetInfo) => {
-          if (assetInfo.name.endsWith('.css')) {
-            return 'assets/css/[name].[ext]'
-          }
-          return 'assets/[name].[ext]'
-        },
-        chunkFileNames: 'assets/js/[name].[hash].js',
-        entryFileNames: 'assets/js/[name].[hash].js'
-      }
-    }
-  },
-  server: {
-    port: 5173,
-    host: true
-  },
-  preview: {
-    port: 5173,
-    host: true
+    sourcemap: false
   }
 }) 

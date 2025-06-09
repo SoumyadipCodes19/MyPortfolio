c34c1e5f27e5df760a572ac5e6b5a323954cb0ca
SoumyadipCodes19 <soumyadip.ghosh19082004@gmail.com>
2025-06-09 19:05:14 +0530

    fix: add vercel config and update build settings

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   build: {
     outDir: 'dist',
     sourcemap: false,
@@ -26,10 +27,10 @@ export default defineConfig({
   },
   server: {
     port: 5173,
-    strictPort: true,
+    host: true
   },
   preview: {
     port: 5173,
-    strictPort: true,
+    host: true
   }
 }) 
